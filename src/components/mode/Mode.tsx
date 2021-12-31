b8b6fd8c12a2c80c32b97f7bbd335d219ec23824
Song <misosjm@naver.com>
2021-12-31 19:28:40 +0900

    기능들

---
+++ src/components/mode/Mode.tsx
@@ -0,0 +1,9 @@
+const Mode = () => {
+    const handleClick = (e) => {}
+    return (
+        <>
+            <input type='button' value='하드모드' onClick={handleClick} />
+        </>
+    )
+}
+export default Mode
